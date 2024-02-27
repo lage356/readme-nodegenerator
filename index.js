35c21dd9a6b557b00537facbd6f13c90c1f69410
lage356 <fernando.lage356@gmail.com>
2024-02-26 16:19:42 -0800

    Adding functionality with js, now works to generate readme

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMark =require ('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMark = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide short description of the project",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "How to install?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do i use the app?",
+  },
+  {
+    type: "input",
+    name: "credit",
+    message: "Who is getting the credit?",
+  },
+  {
+    type: "list",
+    message: "Is your app licensed? Please select one of the list",
+    name: "license",
+    choices: [
+
+      "Apache 2.0",
+      ,
+      "BSD-3 Clause",
+      ,
+      "CC0",
+      ,
+      "GNU GPL v3",
+      ,
+      "The Hippocratic License 2.1",
+      ,
+      "IBM Public Lincense 1.0",
+      ,
+      "MIT",
+      ,
+      "The Unlicense",
+      "no license",
+    ],
+    validate(answer) {
+      if (answer.length < 1) {
+        return "You most choose one, if no license select The Unlicense";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "What are the features of this app",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successfully created readmeFile!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const content = generateMark(data);
+    writeToFile("README.MD", content);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  if (license !== 'no license') {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}]`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if (license !== 'no license') {
+    return `- [License](#license)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  if (license !== 'no license') {
+    return `## License
+    the license info goes here`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = ({
+  title,
+  description,
+  install,
+  usage,
+  credit,
+  license,
+  features,
+}) =>
+  `# ${title} 
+  ${renderLicenseBadge(license)}
+  ${renderLicenseLink(license)}
 
-`;
-}
+  ## Description
+   
+  ${description}
+
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+
+  ${install}
+
+  ## Usage
+
+  ${usage}
+
+
+  ## Credits
+  
+  ${credit}
+  
+  ${renderLicenseSection(license)}
+
+  ## Features
+  
+ ${features}
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  
+  `;
 
 module.exports = generateMarkdown;
